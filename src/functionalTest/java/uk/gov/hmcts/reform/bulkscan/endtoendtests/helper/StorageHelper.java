1d53329693f2d0257aaa42e0f72db505a34e4746
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2020-06-03 11:42:05 +0100

    Use reform  proxy to upload to blob storage (#38)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/StorageHelper.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/StorageHelper.java
@@ -1,14 +1,21 @@
 package uk.gov.hmcts.reform.bulkscan.endtoendtests.helper;
 
+import com.azure.core.http.ProxyOptions;
+import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
 import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.typesafe.config.ConfigFactory;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.SasTokenRetriever;
 
 import java.io.ByteArrayInputStream;
+import java.net.InetSocketAddress;
 
 public final class StorageHelper {
 
     private static final String storageUrl = ConfigFactory.load().getString("storage-account-url");
+
+    private static final String proxyHost = ConfigFactory.load().getString("proxy-host");
+
+    private static final int proxyPort = ConfigFactory.load().getInt("proxy-port");
 
     private StorageHelper() {
         // utility class
@@ -20,6 +27,13 @@ public final class StorageHelper {
         new BlobContainerClientBuilder()
             .endpoint(storageUrl + "/" + container)
             .sasToken(sasToken)
+            .httpClient(new NettyAsyncHttpClientBuilder()
+                            .proxy(new ProxyOptions(
+                                       ProxyOptions.Type.HTTP,
+                                       new InetSocketAddress(proxyHost, proxyPort)
+                                   )
+                            )
+                            .build())
             .buildClient()
             .getBlobClient(fileName)
             .upload(new ByteArrayInputStream(zipFileContent), zipFileContent.length);
