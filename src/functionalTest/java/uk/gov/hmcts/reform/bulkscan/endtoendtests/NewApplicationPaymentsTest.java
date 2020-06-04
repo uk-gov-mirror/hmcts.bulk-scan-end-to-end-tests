22914b101fbc470fad8595b6da1e04bcaf589c8b
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2020-06-04 15:25:43 +0100

    Introduced NewApplicationPaymentsTest (#44)
    * Introduced NewApplicationPaymentsTest
    
    * Making use of Await helper class
    
    * Fixed unsolicited change by intellij
    
    * Removed comments
    
    * Eliminating path from zip file entry

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/NewApplicationPaymentsTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.bulkscan.endtoendtests;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Await;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.StorageHelper;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.ZipFileHelper;
+
+import static java.util.Collections.singletonList;
+
+public class NewApplicationPaymentsTest {
+
+    @Test
+    public void should_upload_blob_and_create_exception_record() throws Exception {
+        String zipFileName = ZipFileHelper.randomFileName();
+
+        var zipArchive = ZipFileHelper.createZipArchive(
+            singletonList("test-data/new-application-payments/1111002.pdf"),
+            "test-data/new-application-payments/metadata.json",
+            zipFileName
+        );
+
+        StorageHelper.uploadZipFile("bulkscan", zipFileName, zipArchive);
+
+        Await.envelopeDispatched(zipFileName);
+        Await.envelopeCompleted(zipFileName);
+    }
+}
