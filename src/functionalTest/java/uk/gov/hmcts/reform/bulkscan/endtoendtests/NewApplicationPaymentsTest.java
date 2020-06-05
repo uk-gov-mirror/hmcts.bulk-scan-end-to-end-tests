37e2156acfd0d718399d7aae049a01e9c6994685
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2020-06-05 13:28:04 +0100

    Added assertions (#77)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/NewApplicationPaymentsTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/NewApplicationPaymentsTest.java
@@ -5,8 +5,11 @@ import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Await;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Container;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.StorageHelper;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.ZipFileHelper;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.ProcessorEnvelopeResult;
 
 import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.ProcessorEnvelopeStatusChecker.getZipFileStatus;
 
 public class NewApplicationPaymentsTest {
 
@@ -22,5 +25,17 @@ public class NewApplicationPaymentsTest {
 
         Await.envelopeDispatched(zipArchive.fileName);
         Await.envelopeCompleted(zipArchive.fileName);
+
+        //get the process result again and assert
+        assertCompletedProcessorResult(zipArchive.fileName);
+    }
+
+    private void assertCompletedProcessorResult(String zipFileName) {
+        ProcessorEnvelopeResult processorEnvelopeResult = getZipFileStatus(zipFileName);
+        assertThat(processorEnvelopeResult.ccdId).isNotBlank();
+        assertThat(processorEnvelopeResult.container).isEqualTo(Container.BULKSCAN.name);
+        assertThat(processorEnvelopeResult.envelopeCcdAction).isEqualTo("EXCEPTION_RECORD");
+        assertThat(processorEnvelopeResult.id).isNotBlank();
+        assertThat(processorEnvelopeResult.status).isEqualTo("COMPLETED");
     }
 }
