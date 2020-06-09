1e6d724d52000749f1a9f8193bf157ec15e1094c
donatas M <1420191+doncem@users.noreply.github.com>
2020-06-09 13:06:06 +0100

    Add `AllServices` test (#68)

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/AllServicesTest.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.bulkscan.endtoendtests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Await;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Container;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.StorageHelper;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.ZipFileHelper;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.model.Classification;
+
+import static uk.gov.hmcts.reform.bulkscan.endtoendtests.model.Classification.EXCEPTION;
+import static uk.gov.hmcts.reform.bulkscan.endtoendtests.model.Classification.NEW_APPLICATION;
+
+class AllServicesTest {
+
+    @ParameterizedTest
+    @MethodSource("serviceWithClassification")
+    void should_create_case_for_service(Container container, Classification classification) throws Exception {
+        // given
+        var zipArchive = ZipFileHelper.createZipArchive("test-data/" + classification, container);
+
+        // when
+        StorageHelper.uploadZipFile(container, zipArchive);
+
+        // then
+        Await.envelopeDispatched(zipArchive.fileName);
+        Await.envelopeCompleted(zipArchive.fileName);
+    }
+
+    private static Object[][] serviceWithClassification() {
+        return new Object[][] {
+            new Object[] { Container.CMC, EXCEPTION },
+            new Object[] { Container.DIVORCE, NEW_APPLICATION },
+            new Object[] { Container.FINREM, NEW_APPLICATION },
+            new Object[] { Container.PROBATE, NEW_APPLICATION },
+            new Object[] { Container.PUBLICLAW, EXCEPTION },
+            new Object[] { Container.SSCS, NEW_APPLICATION }
+        };
+    }
+}
