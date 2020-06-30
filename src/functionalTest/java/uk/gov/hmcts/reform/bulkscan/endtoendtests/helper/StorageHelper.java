7accc211f4ee49e97d9b271111a18d7b3ef24001
mdayican <mustafa.dayican@hmcts.net>
2020-06-30 22:11:13 +0100

    Revert "change content type for upload (#99)" (#100)
    This reverts commit 6af5f55f1ae7f0e8fe6aaba8d782ace895b126f4.

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/StorageHelper.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/StorageHelper.java
@@ -4,11 +4,7 @@ import com.azure.core.http.ProxyOptions;
 import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
-import com.azure.core.util.Context;
 import com.azure.storage.blob.BlobContainerClientBuilder;
-import com.azure.storage.blob.models.BlobHttpHeaders;
-import com.azure.storage.blob.models.BlobRequestConditions;
-import com.azure.storage.common.implementation.Constants;
 import com.typesafe.config.ConfigFactory;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.ZipFileHelper.ZipArchive;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.SasTokenRetriever;
@@ -30,10 +26,6 @@ public final class StorageHelper {
 
     public static void uploadZipFile(Container container, ZipArchive zipArchive) {
         String sasToken = SasTokenRetriever.getTokenFor(container.name);
-        BlobRequestConditions blobRequestConditions = new BlobRequestConditions();
-        blobRequestConditions.setIfNoneMatch(Constants.HeaderConstants.ETAG_WILDCARD);
-
-        BlobHttpHeaders headers = new BlobHttpHeaders();
 
         new BlobContainerClientBuilder()
             .endpoint(storageUrl + "/" + container.name)
@@ -48,17 +40,7 @@ public final class StorageHelper {
             .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
             .buildClient()
             .getBlobClient(zipArchive.fileName)
-            .uploadWithResponse(
-                new ByteArrayInputStream(zipArchive.content),
-                zipArchive.content.length,
-                null,
-                headers.setContentType("application/zip"),
-                null,
-                null,
-                blobRequestConditions,
-                null,
-                Context.NONE
-            );
-
+            .getBlockBlobClient()
+            .upload(new ByteArrayInputStream(zipArchive.content), zipArchive.content.length);
     }
 }
