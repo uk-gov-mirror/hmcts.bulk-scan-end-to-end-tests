c5c28d83d820042f312f734c9f6d664352314f0b
donatas M <1420191+doncem@users.noreply.github.com>
2020-07-01 10:15:30 +0100

    Revert "Add http policy to manually include custom content type header (#101)" (#102)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/StorageHelper.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/StorageHelper.java
@@ -37,15 +37,6 @@ public final class StorageHelper {
                                    )
                             )
                             .build())
-            // each policy is executed before sending out the request
-            .addPolicy(((context, next) -> {
-                context
-                    .getHttpRequest()
-                    .getHeaders()
-                    .put("Content-Type", "application/zip");
-
-                return next.process();
-            }))
             .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
             .buildClient()
             .getBlobClient(zipArchive.fileName)
