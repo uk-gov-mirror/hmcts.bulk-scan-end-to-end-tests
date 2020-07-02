3120b70658eaab17e0a702b8aed3efd5c596ed5e
mdayican <mustafa.dayican@hmcts.net>
2020-07-02 17:08:17 +0100

    Revert "set content type"
    This reverts commit 6c17343472697cb317d3ad74cd8b57d0e6a706bd.

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/client/CcdClient.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/client/CcdClient.java
@@ -128,7 +128,6 @@ public class CcdClient {
         CaseDataContent caseDataContent
     ) {
         return getRequestSpecification(idamToken, s2sToken)
-            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .pathParam("userId", userId)
             .pathParam("jurisdictionId", jurisdictionId)
             .pathParam("caseType", caseType)
