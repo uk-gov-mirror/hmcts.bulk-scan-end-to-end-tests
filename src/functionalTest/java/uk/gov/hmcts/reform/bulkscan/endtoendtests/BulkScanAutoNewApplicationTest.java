50aec8fdafda377c6f2992814958f03f8ae237b3
MUSTAFA <mustafa.dayican@hmcts.net>
2021-04-21 14:11:35 +0100

    add test attachment supplementary evidence to auto created case (#239)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/BulkScanAutoNewApplicationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/BulkScanAutoNewApplicationTest.java
@@ -2,8 +2,13 @@ package uk.gov.hmcts.reform.bulkscan.endtoendtests;
 
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Await;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Container;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.StorageHelper;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.ZipFileHelper;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.model.Classification;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.ProcessorEnvelopeResult;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Container.BULKSCAN_AUTO;
@@ -23,11 +28,55 @@ public class BulkScanAutoNewApplicationTest {
         Await.envelopeCompleted(zipArchive.fileName);
 
         //get the process result again and assert
-        assertCompletedProcessorResult(zipArchive.fileName);
+        assertCompletedAutoCaseResult(getZipFileStatus(zipArchive.fileName));
     }
 
-    private void assertCompletedProcessorResult(String zipFileName) {
+    @Test
+    public void should_create_case_automatically_and_attach_supplementary_evidence()
+        throws Exception {
+
+        var zipArchiveCase = ZipFileHelper.createZipArchive("test-data/new_application", BULKSCAN_AUTO);
+
+        StorageHelper.uploadZipFile(BULKSCAN_AUTO, zipArchiveCase);
+
+        Await.envelopeDispatched(zipArchiveCase.fileName);
+        Await.envelopeCompleted(zipArchiveCase.fileName);
+        // case created automatically
+        //get the process result again and assert
+        Optional<ProcessorEnvelopeResult> optEnvelopeResult
+            = getZipFileStatus(zipArchiveCase.fileName);
+        assertCompletedAutoCaseResult(optEnvelopeResult);
+
+        //attach supplementary evidence
+
+        var zipArchiveSupp = ZipFileHelper.createZipArchive(
+            "test-data/" + Classification.SUPPLEMENTARY_EVIDENCE, Container.BULKSCAN);
+
+        StorageHelper.uploadZipFile(Container.BULKSCAN, zipArchiveSupp);
+
+        Await.envelopeDispatched(zipArchiveSupp.fileName);
+        Await.envelopeCompleted(zipArchiveSupp.fileName);
+
+        //get the process result again and assert
+        assertCompletedSupplementaryResult(
+            zipArchiveSupp.fileName,
+            optEnvelopeResult.get().ccdId
+        );
+
+    }
+
+    private void assertCompletedSupplementaryResult(String zipFileName, String ccdId) {
         assertThat(getZipFileStatus(zipFileName)).hasValueSatisfying(env -> {
+            assertThat(env.ccdId).isEqualTo(ccdId);
+            assertThat(env.container).isEqualTo(Container.BULKSCAN.name);
+            assertThat(env.envelopeCcdAction).isEqualTo("AUTO_ATTACHED_TO_CASE");
+            assertThat(env.id).isNotBlank();
+            assertThat(env.status).isEqualTo("COMPLETED");
+        });
+    }
+
+    private void assertCompletedAutoCaseResult(Optional<ProcessorEnvelopeResult> optEnv) {
+        assertThat(optEnv).hasValueSatisfying(env -> {
             assertThat(env.ccdId).isNotBlank();
             assertThat(env.container).isEqualTo(BULKSCAN_AUTO.name);
             assertThat(env.envelopeCcdAction).isEqualTo("AUTO_CREATED_CASE");
