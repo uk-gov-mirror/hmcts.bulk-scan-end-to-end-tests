3fb676c60912ffda5660788b6f0b53d550fe8883
MUSTAFA <mustafa.dayican@hmcts.net>
2021-04-23 09:01:49 +0100

    add bulkscan envelope id assertions to supplementary evidence test(#243)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/BulkScanAutoNewApplicationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/BulkScanAutoNewApplicationTest.java
@@ -1,15 +1,19 @@
 package uk.gov.hmcts.reform.bulkscan.endtoendtests;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.client.CcdClient;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Await;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.StorageHelper;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.ZipFileHelper;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.model.Classification;
+import uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.EnvelopeAction;
 import uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.ProcessorEnvelopeResult;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.bulkscan.endtoendtests.client.CcdClient.assertCaseEnvelopes;
 import static uk.gov.hmcts.reform.bulkscan.endtoendtests.helper.Container.BULKSCAN_AUTO;
 import static uk.gov.hmcts.reform.bulkscan.endtoendtests.utils.ProcessorEnvelopeStatusChecker.getZipFileStatus;
 
@@ -45,6 +49,15 @@ public class BulkScanAutoNewApplicationTest {
         Optional<ProcessorEnvelopeResult> optEnvelopeResult
             = getZipFileStatus(zipArchiveCase.fileName);
         assertCompletedAutoCaseResult(optEnvelopeResult);
+        var envCreate = optEnvelopeResult.get();
+        Map<String, Object> caseDataCreated =
+            CcdClient.getCaseData(envCreate.ccdId, BULKSCAN_AUTO.idamUserName, BULKSCAN_AUTO.idamPassword);
+        assertCaseEnvelopes(
+            caseDataCreated,
+            new EnvelopeAction[]{
+                new EnvelopeAction(envCreate.id, "create")
+            }
+        );
 
         //attach supplementary evidence
 
@@ -56,6 +69,21 @@ public class BulkScanAutoNewApplicationTest {
 
         Await.envelopeDispatched(zipArchiveSupp.fileName);
         Await.envelopeCompleted(zipArchiveSupp.fileName);
+
+        optEnvelopeResult
+            = getZipFileStatus(zipArchiveSupp.fileName);
+        assertCompletedAutoCaseResult(optEnvelopeResult);
+        envCreate = optEnvelopeResult.get();
+        caseDataCreated =
+            CcdClient.getCaseData(envCreate.ccdId, BULKSCAN_AUTO.idamUserName, BULKSCAN_AUTO.idamPassword);
+        assertCaseEnvelopes(
+            caseDataCreated,
+            new EnvelopeAction[]{
+                new EnvelopeAction(envCreate.id, "create"),
+                new EnvelopeAction(envCreate.id, "update")
+            }
+        );
+
 
         //get the process result again and assert
         assertCompletedSupplementaryResult(
