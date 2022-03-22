63ad44811dbf4cbca0da18b92cd049ea0e480e7b
MUSTAFA <mustafa.dayican@hmcts.net>
2022-03-22 19:45:11 +0300

    Clear up dependencies (#288)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/utils/SasTokenRetriever.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/utils/SasTokenRetriever.java
@@ -2,11 +2,11 @@ package uk.gov.hmcts.reform.bulkscan.endtoendtests.utils;
 
 import io.restassured.RestAssured;
 import org.apache.http.HttpHeaders;
-import uk.gov.hmcts.reform.logging.appinsights.SyntheticHeaders;
 
 import static uk.gov.hmcts.reform.bulkscan.endtoendtests.config.TestConfig.BLOB_ROUTER_URL;
 
 public final class SasTokenRetriever {
+    public static final String SYNTHETIC_TEST_SOURCE = "SyntheticTest-Source";
 
     /**
      * Retrieves SAS token for given service/jurisdiction.
@@ -17,7 +17,7 @@ public final class SasTokenRetriever {
             .relaxedHTTPSValidation()
             .baseUri(BLOB_ROUTER_URL)
             .header(HttpHeaders.CONTENT_TYPE, "application/json")
-            .header(SyntheticHeaders.SYNTHETIC_TEST_SOURCE, "Bulk Scan E2E test")
+            .header(SYNTHETIC_TEST_SOURCE, "Bulk Scan E2E test")
             .get("/token/" + jurisdiction)
             .then()
             .log().body() // debug
